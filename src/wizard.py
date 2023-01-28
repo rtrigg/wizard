283305d955e7b3e85761249a03e861761bdce457
Robert Trigg <coolemailaddress01@icloud.com>
2023-01-28 02:06:22 -0500

    Add shop for wizard items

--- src/wizard.py
+++ src/wizard.py
@@ -1,12 +1,39 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 @dataclass
 class Wizard:
     """Class for keeping track of Wizard."""
     name: str
-    satiety: int
+    inventory: list = field(default_factory=lambda: [])
+    satiety: int = 25
+    gold: int = 25
 
     def eat(self) -> None:
         self.satiety += 10
+
+    def buy_item(self, shop, item) -> None:
+        self.gold -= shop.sell(item)
+        self.inventory.append(item)
+
+
+@dataclass
+class Item:
+    """Class for items and it's properties."""
+    name: str
+    cost: int = 10
+
+
+@dataclass
+class Shop:
+    """Class for keeping track of a shop's wares."""
+    name: str = "George's Imporeatems"
+    gold: int = 100
+    # https://stackoverflow.com/questions/59222110/python-type-hinted-dict-syntax-error-mutable-default-is-not-allowed-use-defaul#63231305
+    items: list[Item] = field(default_factory=lambda: [Item("potion", 10)])
+
+    def sell(self, item) -> int:
+        self.gold += item.cost
+        del self.items[self.items.index(item)]
+        return item.cost
 
 
--- tests/test_wizard.py
+++ tests/test_wizard.py
@@ -1,8 +1,19 @@
-from wizard import Wizard
+from wizard import Wizard, Shop
 
 def test_eat():
     wizard = Wizard("Bob", 0)
     initial_satiety = wizard.satiety
     wizard.eat()
     assert wizard.satiety > initial_satiety, f"Hunger meter not filled after eating. Expected > {initial_satiety}, but got {wizard.satiety}."
+
+
+def test_buy_item():
+    wizard = Wizard("Bob")
+    shop = Shop()
+    initial_inventory = wizard.inventory.copy()
+    item_to_buy = shop.items[0]
+    wizard.buy_item(shop, item_to_buy)
+    assert item_to_buy not in initial_inventory, f"{item_to_buy} not added to inventory after buying"
+    assert item_to_buy in wizard.inventory, f"{item_to_buy} not found in the wizard's inventory after buying"
+    assert item_to_buy not in shop.items, f"{item_to_buy} not removed from the shops's inventory after buying"
 
