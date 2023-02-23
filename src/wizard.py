2a82f7768802bf5b5c4b60544ed2eda97c66bac6
Robert Trigg <coolemailaddress01@icloud.com>
2023-02-22 23:24:16 -0500

    Add interface loop

--- src/wizard.py
+++ src/wizard.py
@@ -1,4 +1,7 @@
 from dataclasses import dataclass, field
+import platform
+import os
+import time
 
 
 @dataclass
@@ -13,10 +16,17 @@ class Wizard:
     gold: int = 25
     alive: bool = True
 
-    def update(self):
+    def update(self, action):
         self.food -= 1
         if self.food < 0 or self.mana < 0:
             self.alive = False
+        match action:
+            case "sleep":
+                self.sleep()
+            case "eat":
+                self.eat()
+            case "cast":
+                self.cast()
 
     def eat(self) -> None:
         self.food += 10
@@ -51,3 +61,30 @@ class Shop:
         self.gold += item.cost
         del self.items[self.items.index(item)]
         return item.cost
+
+
+def main():
+    wizard = Wizard()
+    while wizard.alive:
+        if platform.system() == "Windows":
+            _ = os.system("cls")
+        else:
+            _ = os.system("clear")
+        print(wizard)
+        options = ["sleep", "eat", "cast"]
+        user_input = ""
+        input_message = f"What will {wizard.name} do?\n"
+        for item in options:
+            input_message += f"* {item}\n"
+        input_message += "Your choice: "
+        while user_input.lower() not in options:
+            user_input = input(input_message)
+        print(f"{wizard.name} will " + user_input)
+        time.sleep(2)
+        wizard.update(user_input)
+    else:
+        print(f"Oh no! {wizard.name} has died!")
+
+
+if __name__ == "__main__":
+    main()
